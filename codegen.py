dcad1eef4e5b19400db1fb2a03d249adf2835729
danielcaceres107 <danielcaceres98@hotmail.com>
2023-10-25 19:07:00 -0500

    Hybrid version added

--- codegen.py
+++ codegen.py
@@ -7,7 +7,7 @@ from PIL import Image
 
 ################################## SCRIPT ###########################################
 
-def generate(tipo_extension_seleccionado, codigo_catalogo, id_catalogo, catalog_name, extension_id, webpage, email, phone, brandText, header, logo):
+def generate(tipo_extension_seleccionado, codigo_catalogo, id_catalogo, catalog_name, extension_id, webpage, email, phone, brandText, header, logo, productName=None, partnumber=None, prodimg=None):
 
     # crear variables para diferentes tipos de nombre
     catalog_name_min = catalog_name.lower().replace(" ", "")
@@ -51,11 +51,12 @@ def generate(tipo_extension_seleccionado, codigo_catalogo, id_catalogo, catalog_
         changeLine(ruta_archivo, "ExtensionName", catalog_name)
         changeLine(ruta_archivo, "ExtensionMayusName", catalog_name_may)
         changeLine(ruta_archivo, "extensionnamemin", catalog_name_min)
+        changeLine(ruta_archivo, "catalogLetters", codigo_catalogo)
+
 
         ###### modificar extension.cm ... ######
         if ruta_archivo == r"C:\CetDev\version14.0\extensions\custom\%s\extension.cm" % catalog_name_min:
             changeLine(ruta_archivo, "//generateExtensionId", extension_id)
-            changeLine(ruta_archivo, "catalogLetters", codigo_catalogo)
 
         ##### modificar Header.cm ######
         if ruta_archivo == r"C:\CetDev\version14.0\extensions\custom\%s\header.cm" % catalog_name_min:
@@ -69,9 +70,7 @@ def generate(tipo_extension_seleccionado, codigo_catalogo, id_catalogo, catalog_
             cant_chars = len(brandText)
             if cant_chars > 37 :
                 fragmentos = [brandText[i:i+37] for i in range(0, len(brandText), 37)]
-                print(fragmentos)
                 format_brandText = "\\n".join(fragmentos)
-                print(format_brandText)
                  
             changeLine(ruta_archivo, "brandText", format_brandText)
             changeLine(ruta_archivo, "webpage", webpage)
@@ -107,23 +106,13 @@ def generate(tipo_extension_seleccionado, codigo_catalogo, id_catalogo, catalog_
         changeLine(ruta_archivo, "ExtensionName", catalog_name)
         changeLine(ruta_archivo, "ExtensionMayusName", catalog_name_may)
         changeLine(ruta_archivo, "extensionnamemin", catalog_name_min)
-
+        changeLine(ruta_archivo, "catalogLetters", codigo_catalogo)
+        print(ruta_archivo)
         # Renombrar archivos
         if "ExtensionMayusName" in archivo:
             nuevo_nombre = archivo.replace("ExtensionMayusName", catalog_name_may)
             ruta_nuevo_archivo = os.path.join(carpeta_secondtab, nuevo_nombre)
             os.rename(ruta_archivo, ruta_nuevo_archivo)
-
-        ###### modificar EXTENSION.CM... ######
-        if ruta_archivo == r"C:\CetDev\version14.0\extensions\custom\%s\secondTab\extension.cm" % catalog_name_min:
-            changeLine(ruta_archivo, "catalogLetters", codigo_catalogo)
-        ###### modificar library.cm  ########
-        if ruta_archivo == r"C:\CetDev\version14.0\extensions\custom\%s\secondTab\library.cm" % catalog_name_min:
-            changeLine(ruta_archivo, "catalogLetters", codigo_catalogo)
-
-        ###### modificar lazy.xml  ########
-        if ruta_archivo == r"C:\CetDev\version14.0\extensions\custom\%s\secondTab\lazy.xml" % catalog_name_min:
-            changeLine(ruta_archivo, "catalogLetters", codigo_catalogo)
 
         ###### modificar logo.png  ########
         carpeta_destino = r"C:\CetDev\version14.0\extensions\custom\%s\secondTab" % catalog_name_min
@@ -136,16 +125,39 @@ def generate(tipo_extension_seleccionado, codigo_catalogo, id_catalogo, catalog_
 
         ###### buscar archivos de Hybrid #######
         if tipo_extension_seleccionado == "hybrid":
-            if ruta_archivo == r"C:\CetDev\version14.0\extensions\custom\%s\secondTab\prdctLibraryFilters.cm" % catalog_name_min:
-                changeLine(ruta_archivo, "catalogLetters", codigo_catalogo)
-            if ruta_archivo == r"C:\CetDev\version14.0\extensions\custom\%s\secondTab\prdctFilteredLibraryLimb.cm" % catalog_name_min:
-                changeLine(ruta_archivo, "catalogLetters", codigo_catalogo)
-            if ruta_archivo == r"C:\CetDev\version14.0\extensions\custom\%s\secondTab\prdctFilteredVisibility.cm" % catalog_name_min:
-                changeLine(ruta_archivo, "catalogLetters", codigo_catalogo)
+            
+            product_name_min = productName.lower().replace(" ", "")
+            product_name_may = productName.capitalize()
 
+            # Renombrar archivos
+            product_class = codigo_catalogo + product_name_may
+            if "catalogLettersProductNameMay" in archivo:
+                changeLine(ruta_archivo, "productName", productName)
+                changeLine(ruta_archivo, "partnumber", partnumber)
+                changeLine(ruta_archivo, "ProductNameMay", product_name_may)
+                nuevo_nombre = archivo.replace("catalogLettersProductNameMay", product_class)
+                ruta_nuevo_archivo = os.path.join(carpeta_secondtab, nuevo_nombre)
+                os.rename(ruta_archivo, ruta_nuevo_archivo)
+
+            if ruta_archivo == r"C:\CetDev\version14.0\extensions\custom\%s\secondTab\prdctFilteredVisibility.cm" % catalog_name_min:
+                changeLine(ruta_archivo, "productnamemin", product_name_min)
+            if ruta_archivo == r"C:\CetDev\version14.0\extensions\custom\%s\secondTab\prdctLibraryFilters.cm" % catalog_name_min:
+                changeLine(ruta_archivo, "productnamemin", product_name_min)
+            if ruta_archivo == r"C:\CetDev\version14.0\extensions\custom\%s\secondTab\productsHeader.cm" % catalog_name_min:
+                changeLine(ruta_archivo, "productnamemin", product_name_min)
+            if ruta_archivo == r"C:\CetDev\version14.0\extensions\custom\%s\secondTab\productsLibrary.cm" % catalog_name_min:
+                changeLine(ruta_archivo, "partnumber", partnumber)
+                changeLine(ruta_archivo, "productName", productName)
+                changeLine(ruta_archivo, "productnamemin", product_name_min)
+                changeLine(ruta_archivo, "ProductNameMay", product_name_may)
+            if ruta_archivo == r"C:\CetDev\version14.0\extensions\custom\%s\secondTab\localize.rs" % catalog_name_min:
+                changeLine(ruta_archivo, "productName", productName)
+                changeLine(ruta_archivo, "productnamemin", product_name_min)
 
     # IMAGES
     carpeta_images = nueva_carpeta + "\images"
+
+    # poner header #
     shutil.copy(header, carpeta_images) #copiar header dado a carpeta de destino
     # ruta completa del header actual
     ruta_archivo_header = os.path.join(carpeta_images, os.path.basename(header))
@@ -156,6 +168,8 @@ def generate(tipo_extension_seleccionado, codigo_catalogo, id_catalogo, catalog_
     #cambiar nombre 
     os.rename(ruta_archivo_header, nuevo_nombre_archivo)
 
+    # poner imagen producto #
+    shutil.copy(prodimg, carpeta_images)
 
 def changeLine(ruta_archivo, last_line, new_line):
     #leer el contenido
@@ -170,7 +184,7 @@ def changeLine(ruta_archivo, last_line, new_line):
     
 ######################################## END SCRIPT ##########################################
 
-######################################## FORMULARIO ##########################################
+######################################## FORMULARIO BACK ##########################################
 id_example = "Ex: \n // ID1=5589525715858500640 \n // ID2=1755369451227214228 \n // ID3=5327118955185941208 \n extensionId.id0 = int64(0x4d91fc06, 0x24a82c20); \n extensionId.id1 = int64(0x185c5337, 0x2df87194); \n extensionId.id2 = int64(0x49edba77, 0x281baad8); \n extensionId.id3 = int64(0x26a5800d, 0x99f4cb45);"
 def on_entry_focus_in(event):
     if entrada_codigo_catalogo.get() == "Ex: BPG":
@@ -215,6 +229,11 @@ def seleccionar_logo():
     entrada_ruta_logo.delete(0, tk.END)
     entrada_ruta_logo.insert(0, ruta_imagen)
 
+def seleccionar_prodimg(entrada_ruta_prodimg):
+    ruta_imagen = filedialog.askopenfilename(filetypes=[("Archivos de imagen", "*.jpg;*.jpeg;*.png")])
+    entrada_ruta_prodimg.delete(0, tk.END)
+    entrada_ruta_prodimg.insert(0, ruta_imagen)
+
 
 def desactivar_tabs(notebook):
     s = ttk.Style()
@@ -223,17 +242,71 @@ def desactivar_tabs(notebook):
     notebook.enable_traversal()  # Desactivar la navegación por teclado en los tabs
 
 # Función para cambiar de página
-def cambiar_pagina():
-    if notebook.index(notebook.select()) == 0:
-        notebook.hide(0)
-        notebook.select(1)
-        boton_cambiar_pagina.config(text="Siguiente")
-    else:
-        notebook.hide(1)
-        notebook.select(0)
-        boton_cambiar_pagina.config(text="Cambiar página")
+def ir_a_pagina_siguiente(notebook):
+    # Obtener el índice de la página actual
+    indice_actual = notebook.index(notebook.select())
+    
+    # Calcular el índice de la página siguiente (sumando 1)
+    indice_siguiente = min(notebook.index('end'), indice_actual + 1)  # no ir más allá de la última página
+    
+    # Seleccionar la página siguiente
+    notebook.select(indice_siguiente)
 
-def enviar():
+    #esconder pagina
+    notebook.hide(indice_actual)
+
+def ir_a_pagina_anterior(notebook):
+    # Obtener el índice de la página actual
+    indice_actual = notebook.index(notebook.select())
+
+    # Calcular el índice de la página anterior (restando 1)
+    indice_anterior = max(0, indice_actual - 1)  # no ir por debajo de 0
+
+    # Seleccionar la página anterior
+    notebook.select(indice_anterior)
+
+    #esconder pagina
+    notebook.hide(indice_actual)
+
+def abrir_pagina_hybrid():
+    #crear pagina 3
+    page3 = tk.Frame(notebook, bg="#939494")
+    page3.pack(fill="both", expand=True)
+
+    # titulo pagina 3
+    etiqueta_title3 = tk.Label(page3, text="AÑADE UN ITEM", bg="#939494", fg="white", font= font.Font(family="Roboto", size=12, weight='bold'))
+    etiqueta_title3.pack(pady=4)
+    
+    # label partnumber
+    etiqueta_partnumber = tk.Label(page3, text="PN:", bg="#939494", fg="white", font=("Roboto", 12))
+    etiqueta_partnumber.pack(pady=4)
+    
+    entrada_partnumber = tk.Entry(page3, width=40, font=("Roboto", 12))
+    entrada_partnumber.pack(pady=10)
+
+    #label productName
+    etiqueta_productName = tk.Label(page3, text="Product Name:", bg="#939494", fg="white", font=("Roboto", 12))
+    etiqueta_productName.pack(pady=4)
+    
+    entrada_productName = tk.Entry(page3, width=40, font=("Roboto", 12))
+    entrada_productName.pack(pady=10)
+
+    # Campo seleccionar imagen producto
+    etiqueta_ruta_prodimg = tk.Label(page3, text="Seleccionar imagen (60X60)", bg="#939494", fg="white", font=("Roboto", 12))
+    etiqueta_ruta_prodimg.pack()
+
+    entrada_ruta_prodimg = tk.Entry(page3, width=40, font=("Roboto", 12))
+    entrada_ruta_prodimg.pack(pady=5)
+
+    # Botón para seleccionar imagen producto
+    boton_seleccionar_prodimg = tk.Button(page3, text="Seleccionar imagen", command=lambda: seleccionar_prodimg(entrada_ruta_prodimg), bg="#7E8D8D", fg="black", font=fuente_title)
+    boton_seleccionar_prodimg.pack(pady=10)
+
+    # Botón para enviar
+    boton_enviar = tk.Button(page3, text="Enviar", command=lambda: enviar(entrada_productName.get(), entrada_partnumber.get(), entrada_ruta_prodimg.get()), bg="#7E8D8D", fg="black", font=fuente_title)
+    boton_enviar.pack(pady=20)
+
+def enviar(entrada_productName=None, entrada_partnumber=None, entrada_ruta_prodimg=None):
     tipo_extension_seleccionado = combo_tipo_extension.get()
     codigo_catalogo = entrada_codigo_catalogo.get()
     id_catalogo = entrada_id_catalogo.get()
@@ -245,14 +318,23 @@ def enviar():
     brandText = entrada_brand_text.get("1.0", "end-1c")
     header = entrada_ruta_header.get()
     logo = entrada_ruta_logo.get()
+
+    if tipo_extension_seleccionado == "hybrid":
+        productName = entrada_productName
+        partnumber = entrada_partnumber
+        prodimg = entrada_ruta_prodimg
+    else:
+        productName = None
+        partnumber = None
+        prodimg = None
     
-    generate(tipo_extension_seleccionado, codigo_catalogo, id_catalogo, catalog_name, extension_id, webpage, email, phone, brandText, header, logo)
+    generate(tipo_extension_seleccionado, codigo_catalogo, id_catalogo, catalog_name, extension_id, webpage, email, phone, brandText, header, logo, productName, partnumber, prodimg)
     print("GENERACION EXITOSA: Tipo de extensión seleccionado:", tipo_extension_seleccionado, 
           "Codigo catalogo:", codigo_catalogo, ",",
           "ID catalogo:", id_catalogo, ",",
           "Nombre del catalogo", catalog_name)
     
-
+######################################## FORMULARIO DESIGN ##########################################
 # Crear ventana principal
 myWindow = tk.Tk()
 myWindow.geometry("650x690")
@@ -360,8 +442,12 @@ notebook.add(page2, text="Página 2")
 notebook.hide(1)
 
 # Botón "Cambiar página"
-boton_cambiar_pagina = tk.Button(page1, text="Siguiente", command=cambiar_pagina, bg="#7E8D8D", fg="black", font=fuente_title)
+boton_cambiar_pagina = tk.Button(page1, text="Siguiente", command=lambda: ir_a_pagina_siguiente(notebook), bg="#7E8D8D", fg="black", font=fuente_title)
 boton_cambiar_pagina.pack(pady=20)
+
+# Botón atrás
+boton_atras = tk.Button(page2, text="←", command=lambda: ir_a_pagina_anterior(notebook), bg="#7E8D8D", fg="black", font=fuente_title)
+boton_atras.pack(side="left", anchor="nw", padx=0, pady=0)
 
 # webpage
 etiqueta_webpage = tk.Label(page2, text="Webpage:", bg="#939494", fg="white", font=("Roboto", 12))
@@ -414,9 +500,26 @@ entrada_ruta_logo.pack(pady=5)
 boton_seleccionar_logo = tk.Button(page2, text="Seleccionar imagen", command=seleccionar_logo, bg="#7E8D8D", fg="black", font=fuente_title)
 boton_seleccionar_logo.pack(pady=10)
 
-# Botón para enviar
-boton_tipo_extension = tk.Button(page2, text="Enviar", command=enviar, bg="#7E8D8D", fg="black", font=fuente_title)
-boton_tipo_extension.pack(pady=20)
+def actualizar_boton():
+    tipo_extension_seleccionado = combo_tipo_extension.get()
+    print(tipo_extension_seleccionado)
+    if tipo_extension_seleccionado == "hybrid":
+        boton_enviar.pack_forget()  # Oculta el botón "Enviar"
+        boton_siguiente.pack()  # Muestra el botón "Siguiente"
+    else:
+        boton_siguiente.pack_forget()  # Oculta el botón "Siguiente"
+        boton_enviar.pack()  # Muestra el botón "Enviar"
+
+# Crea el botón "Enviar"
+boton_enviar = tk.Button(page2, text="Enviar", command=enviar, bg="#7E8D8D", fg="black", font=fuente_title)
+
+# Crea el botón "Siguiente"
+boton_siguiente = tk.Button(page2, text="Siguiente", command=abrir_pagina_hybrid, bg="#7E8D8D", fg="black", font=fuente_title)
+
+actualizar_boton()
+
+# Conectar la función `actualizar_boton` con el evento de selección en el combo_box
+combo_tipo_extension.bind("<<ComboboxSelected>>", lambda e: actualizar_boton())
 
 # Configurar la barra de desplazamiento
 scrollbar.config(command=canvas.yview)
